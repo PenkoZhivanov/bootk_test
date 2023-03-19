76541a68ccabd3d4d3945b7cca81dfa7a52e9d2b
jivanov@ucc.uni-sofia.bg <jivanov@ucc.uni-sofia.bg>
2023-03-19 15:49:20 +0200

    update

---
+++ config.js
@@ -0,0 +1,7 @@
+const bar = "¤";
+var inventar_mass = 20;
+const teglo = 1; // индекс в масива
+const item_name = 0;
+const  visible = 4;
+const broi = 2;
+const item_detail = 3;
--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-const bar = "¤";
+var episode = 1;
 var hero = new Hero();
-hero.create(10,16,25,30,10);
+hero.create(10, 16, 25, 30, 10);
 hero.showBars();
- console.log(sets["ep12"][0].nm);
-document.getElementById("found").innerHTML=sets["ep12"][0].nm;
-var output = "<table id='table_found' onmouseleave='tt(2);'>";
-for(var i=0;i<sets["ep12"].length;i++) {
-    output = `${output}<tr ><td class="bor" onmouseover="tt(1,'${sets["ep12"][i].tt}');"> ${sets["ep12"][i].nm}</td><td > ${sets["ep12"][i].teglo}</td><td><button class='get' onclick='addToInventar(${i})'>Вземи</button></td></tr>`;
-}
-found.innerHTML=output+"</table>";
-console.log(output);
- 
 var subject = new Thing();
-subject.construct(1,2,3,4,5,6,7,8,9,10,11,12);
+subject.construct(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+go(1);
+
 
 // for test only !
-var episode = '11';
-r_content.innerHTML= episodes[episode];
-r_content_th.innerHTML="Епизод "+episode;
-function addToInventar(){
 
-} 
-function tt(vis,episode){
-    console.log(episode);
-   vis==1?document.getElementById("tooltip").innerHTML= episode:document.getElementById("tooltip").innerHTML="";  
+r_content.innerHTML = episodes[episode];
+r_content_th.innerHTML = "Епизод " + episode;
+
+ showInventar();  
+
+function tt(vis, episode) {
+    vis == 1 ? document.getElementById("tooltip").innerHTML = episode: document.getElementById("tooltip").innerHTML = "";
 }
-function addToInventar(item){
-    
+
+function addToInventar(item) {
+    let sum=0;
+    inventar.forEach(element=>{sum+=element[teglo];})
+    if(sum+sets[episode][item][teglo]*sets[episode][item][broi]>inventar_mass){
+        alert('Прекалено тежко!');
+        sum=0;
+        return;
+    }
+   
+    inventar.push([sets[episode][item][item_name],sets[episode][item][teglo],sets[episode][item][broi]]);
+    sets[episode][item][visible]=0;
+    go(episode);
+    showInventar();    
 }
-function go(episode){
+
+
+function go(episode) {
+    if (sets[episode]) {
+     
+        var output = "<table id='table_found' onmouseleave='tt(2);'><tr><th>Име</th><th>Тегло</th><th>Брой</th><th></th></tr>";
+        for (var i = 0; i < sets[episode].length; i++) {
+            if(sets[episode][i][visible]==0) continue;
+            output = `${output}<tr ><td class="bor" onmouseover="tt(1,'${sets[episode][i][3]}');"> ${sets[episode][i][0]}</td><td > ${sets[episode][i][teglo]}</td><td > ${sets[episode][i][broi]}</td><td><button class='get' onclick='addToInventar(${i})'>Вземи</button></td></tr>`;
+        }
+        found.innerHTML = output + "</table>";
+    }
+}
+
+function showInventar(){
+   let output =  "<table id='table_inventar' onmouseleave='tt(2);'>";
+    //inventar.forEach(element=>{sum+=element[teglo];});
+    for (let i = 0; i < inventar.length; i++) {
+        output = `${output}<tr ><td class="bor" onmouseover="tt(1,'${inventar[i][3]}');"> ${inventar[i][0]}</td><td > ${inventar[i][teglo]}</td><td><button class='get' onclick='removeFromInventar(${i})'>Изхвърли</button></td></tr>`;
+    }
     
+    inventar_table.innerHTML=output+"</table>";
 }
